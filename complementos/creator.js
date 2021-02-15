4e763ce97a4581744c564698b4211aa07094c31c
CiverStorn <67031366+CiverStorn@users.noreply.github.com>
2021-02-14 20:41:24 -0400

    Delete complementos directory

--- complementos/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '584123257015' , 'josue',m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
